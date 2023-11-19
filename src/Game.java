8805b7eb69ca5ecbc1421d2bbc6d24b3e19460ae
bergandara <gandaraber@gmail.com>
2023-11-19 21:59:18 +0000

    Game class and attributes created

---
+++ src/Game.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+import java.util.Scanner;
+
+public class Game {
+    static Scanner input = new Scanner(System.in);
+    private Grid grid;
+    private int connectN;
+    private Player[] players;
+    private Map<String, Integer> score;
+    private int targetScore;
+
+
+}
