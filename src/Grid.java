f3c1009d5fb7214449f4b245255c533f773a9949
bergandara <gandaraber@gmail.com>
2023-11-19 21:10:43 +0000

    Grid constructor and init methods added

--- src/Grid.java
+++ src/Grid.java
@@ -1,2 +1,20 @@
 public class Grid {
+    private int rows;
+    private int columns;
+    private int[][] grid;
+
+    public Grid(int rows, int columns){
+        this.rows = rows;
+        this.columns = columns;
+        initGrid();
+    }
+
+    public void initGrid(){
+        this.grid = new int[rows][columns];
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
+                grid[i][j] = GridPosition.EMPTY.ordinal();
+            }
+        }
+    }
 }
