af4285cf6a5b169fd533aeebcde8794ccca3a79a
bergandara <gandaraber@gmail.com>
2023-11-19 22:16:13 +0000

    Working in the Game class

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -9,5 +10,27 @@ public class Game {
     private Map<String, Integer> score;
     private int targetScore;
 
+    public Game(Grid grid, int connectN, int targetScore){
+        this.grid = grid;
+        this.connectN = connectN;
+        this.targetScore = targetScore;
 
+        this.players = new Player[]{
+                new Player("Player 1", GridPosition.YELLOW);
+                new Player("Player 2", GridPosition.RED);
+        };
+
+        this.score = new HashMap<>();
+        for(Player player : this.players){
+            this.score.put(player.getName(), 0);
+        }
+    }
+
+//    private void printBoard(){
+//        System.out.println("Board:");
+//        for(int i = 0; i < grid.length; i++){
+//            String row ="";
+//            for(int piece : )
+//        }
+//    }
 }
