2968c1d017b9e2f8dbde68a40f4659bbc474c3db
bergandara <gandaraber@gmail.com>
2023-11-20 18:29:19 +0000

    methods created: printBoard(), playMove(), playRound()

--- src/Game.java
+++ src/Game.java
@@ -15,9 +15,9 @@ public class Game {
         this.connectN = connectN;
         this.targetScore = targetScore;
 
-        this.players = new Player[]{
-                new Player("Player 1", GridPosition.YELLOW);
-                new Player("Player 2", GridPosition.RED);
+        this.players = new Player[] {
+                new Player("Player 1", GridPosition.YELLOW),
+                new Player("Player 2", GridPosition.RED)
         };
 
         this.score = new HashMap<>();
@@ -26,11 +26,49 @@ public class Game {
         }
     }
 
-//    private void printBoard(){
-//        System.out.println("Board:");
-//        for(int i = 0; i < grid.length; i++){
-//            String row ="";
-//            for(int piece : )
-//        }
-//    }
+    private void printBoard(){
+        System.out.println("Board:");
+        int[][] grid = this.grid.getGrid();
+        for(int i = 0; i < grid.length; i++){
+            String row ="";
+            for(int piece : grid[i]){
+                if(piece == GridPosition.EMPTY.ordinal()){
+                    row += "0 ";
+                }else if (piece == GridPosition.YELLOW.ordinal()){
+                    row += "Y ";
+                }else if (piece == GridPosition.RED.ordinal()){
+                    row += "R ";
+                }
+            }
+            System.out.println(row);
+        }
+        System.out.println();
+    }
+
+    private int[] playMove(Player player){
+        printBoard();
+        System.out.println(player.getName() + "'s turn");
+        int colCnt = this.grid.getColumnCount();
+
+        System.out.println("Enter column between 0 and " + (colCnt - 1) + " t add piece : ");
+        int moveColumn = input.nextInt();
+        int moveRow = this.grid.placePiece(moveColumn, player.getPieceColor());
+        return new int[] { moveRow, moveColumn};
+    }
+
+    private Player playRound(){
+        while(true){
+            for(Player player : this.players){
+                int[] pos = playMove(player);
+                int row = pos[0];
+                int col = pos[1];
+                GridPosition pieceColor = player.getPieceColor();
+                if(this.grid.checkWin(this.connectN, row, col, pieceColor)){
+                    this.score.put(player.getName(), this.score.get(player.getName()) + 1);
+                    return player;
+                }
+            }
+        }
+    }
+
 }
